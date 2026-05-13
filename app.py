04ebceacd73c4e80291137a7a1adae937423de20
Komal Joshi <moudgillkomal6@gmail.com>
2026-05-13 13:22:39 +0530

    new commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Docker CI/CD!"
+    return "Hello from Docker CI/CD! First Practical Executed Successfully"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
